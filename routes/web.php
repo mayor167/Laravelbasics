8c4f1eca861ed13c11ea62525fe1e6868b327c12
mayor167 <adenijimayokun@gmail.com>
2025-04-19 17:44:11 +0100

    grouped route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,25 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return "09063676340";
 });
+Route::get('/portifolio', function(){
+    return view('portifolio');
+});
+//parameter using route
+Route::get('/portifolio/{firstname}/{lastname}', function ($firstname, $lastname){
+    // return view('portifolio');
+    return "{$firstname}  {$lastname}";
+});
 //named route
 Route::get('/test', function(){
     return "This is a test page";
 })->name("testpage");
+
+//grouped route
+Route::prefix('portifolio')->group(function(){
+    Route::get('/company', function(){
+        return view('company');
+    });
+    Route::get('/about-us', function(){
+        return view('about-us');
+    });
+});
